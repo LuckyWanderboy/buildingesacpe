b9935d50563c51835bb22b9e6805574d80bc9f28
LuckyWanderboy <ganlaiwang@gmail.com>
2024-04-25 11:53:00 -0700

    halfway through

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PositionReport.h"
+
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -19,7 +18,14 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));
+	
+	FString ObjectLabelName = GetOwner()->GetActorNameOrLabel();
+	FString ObjectName = GetOwner()->GetName();
+
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s %s at %s"), *ObjectName, *ObjectLabelName, *ObjectPos);
+	
 	
 }
 
